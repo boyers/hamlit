6fb6444dd575b66f584f1b7cc1e7b8f285eca52f
Stephan Boyer <stephan@stephanboyer.com>
2014-11-11 20:03:50 -0500

    password fields now have the correct type

--- views/log_in.jsx
+++ views/log_in.jsx
@@ -10,7 +10,7 @@ var LogIn = React.createClass({
             <div className="span4 offset4">
               <Form ref="form" title="Welcome back!" submitText="Log in" url="/api/log_in" fields={[
                 <Input id="email" label="Email" placeholder="hello@example.com" />,
-                <Input id="password" label="Password" placeholder="eigenpassword" btw="Click &lt;a href=&quot;/&quot;>here&lt;/a&gt; if you forgot it." />
+                <Input id="password" label="Password" type="password" placeholder="eigenpassword" btw="Click &lt;a href=&quot;/&quot;>here&lt;/a&gt; if you forgot it." />
               ]} />
             </div>
           </div>
