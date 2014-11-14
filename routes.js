1bbe6fcbb59a23f4d749c704fad320d1cca77041
Stephan Boyer <stephan@stephanboyer.com>
2014-11-14 02:08:09 -0500

    minor refactoring

--- routes.js
+++ routes.js
@@ -1,12 +1,14 @@
 var assets = require('./assets');
 
+var htmlRoutes = /^\/(about)?$/;
+
 exports.config = function(app) {
   if (process.env.NODE_ENV === 'production') {
-    app.get(/^\/(about)?$/, function(req, res) {
+    app.get(htmlRoutes, function(req, res) {
       res.sendFile(__dirname + '/public/assets/application.html');
     });
   } else {
-    app.get(/^\/(about)?$/, function(req, res) {
+    app.get(htmlRoutes, function(req, res) {
       res.render('application', {
         styles: assets.getStyles(),
         scripts: assets.getScripts()
