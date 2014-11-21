06696d9518ec7f8f4041de621e4f3dcd2f872468
Stephan Boyer <stephan@stephanboyer.com>
2014-11-21 07:47:05 -0500

    better error handling ux and some refactoring

--- routes.js
+++ routes.js
@@ -79,19 +79,127 @@ exports.config = function(app) {
     });
   });
 
-  app.post('/api/log_out', function(req, res) {
-    if (req.signedCookies.sessionId) {
-      models.Session.remove({ _id: req.signedCookies.sessionId }, function(err) {
+  app.post('/api/sign_up', function(req, res) {
+    var username = req.body.username.replace(/^\s+|\s+$/g, '');
+    var password = req.body.password;
+    var verifyPassword = req.body.verifyPassword;
+
+    if (username === '') {
+      return res.json({
+        error: null,
+        validationErrors: {
+          username: 'Please enter a username.'
+        }
+      });
+    }
+
+    if (!models.User.validateUsername(username)) {
+      return res.json({
+        error: null,
+        validationErrors: {
+          username: 'That username doesn&rsquo;t look valid. Please use only letters, digits, and underscores.'
+        }
+      });
+    }
+
+    models.User.findOne({
+        usernameLowercase: username.toLowerCase()
+      }, function(err, existingUser) {
+      if (err) {
+        return renderError(res, err);
+      }
+
+      if (existingUser) {
+        return res.json({
+          error: null,
+          validationErrors: {
+            username: 'That username is already taken.'
+          }
+        });
+      }
+
+      if (password === '') {
+        return res.json({
+          error: null,
+          validationErrors: {
+            password: 'Please enter a password.'
+          }
+        });
+      }
+
+      if (verifyPassword === '') {
+        return res.json({
+          error: null,
+          validationErrors: {
+            verifyPassword: 'Please re-enter your password here.'
+          }
+        });
+      }
+
+      if (password !== verifyPassword) {
+        return res.json({
+          error: null,
+          validationErrors: {
+            verifyPassword: 'Your passwords do not match.'
+          }
+        });
+      }
+
+      bcrypt.genSalt(bcryptRounds, function(err, salt) {
         if (err) {
           return renderError(res, err);
         }
 
-        res.clearCookie('sessionId');
-        return res.json({
-          error: null
+        bcrypt.hash(password, salt, function(err, hash) {
+          if (err) {
+            return renderError(res, err);
+          }
+
+          var user = new models.User({
+            username: username,
+            usernameLowercase: username.toLowerCase(),
+            passwordHash: hash,
+            passwordSalt: salt
+          });
+
+          user.save(function(err) {
+            if (err) {
+              if (err.code === 11000) {
+                return res.json({
+                  error: null,
+                  validationErrors: {
+                    username: 'That username is already taken.'
+                  }
+                });
+              }
+
+              return renderError(res, err);
+            }
+
+            var session = new models.Session({
+              data: { userId: user._id }
+            });
+
+            session.save(function(err) {
+              if (err) {
+                return renderError(res, err);
+              }
+
+              res.cookie('sessionId', session.id, {
+                signed: true,
+                maxAge: constants.sessionTTL * 1000
+              });
+
+              return res.json({
+                error: null,
+                validationErrors: { },
+                user: getUserData(user)
+              });
+            });
+          });
         });
       });
-    }
+    });
   });
 
   app.post('/api/log_in', function(req, res) {
@@ -103,15 +211,6 @@ exports.config = function(app) {
         error: null,
         validationErrors: {
           username: 'Please enter your username.'
-        }
-      });
-    }
-
-    if (!models.User.validateUsername(username)) {
-      return res.json({
-        error: null,
-        validationErrors: {
-          username: 'That username doesn&rsquo;t look valid. Please use only letters, digits, and underscores.'
         }
       });
     }
@@ -169,108 +268,34 @@ exports.config = function(app) {
     });
   });
 
-  app.post('/api/sign_up', function(req, res) {
-    var username = req.body.username.replace(/^\s+|\s+$/g, '');
-    var password = req.body.password;
-    var verifyPassword = req.body.verifyPassword;
-
-    if (username === '') {
-      return res.json({
-        error: null,
-        validationErrors: {
-          username: 'Please enter a username.'
-        }
-      });
-    }
-
-    if (!models.User.validateUsername(username)) {
-      return res.json({
-        error: null,
-        validationErrors: {
-          username: 'That username doesn&rsquo;t look valid. Please use only letters, digits, and underscores.'
-        }
-      });
-    }
-
-    if (password === '') {
-      return res.json({
-        error: null,
-        validationErrors: {
-          password: 'Please enter a password.'
-        }
-      });
-    }
-
-    if (verifyPassword === '') {
-      return res.json({
-        error: null,
-        validationErrors: {
-          verifyPassword: 'Please re-enter your password here.'
-        }
-      });
-    }
-
-    if (password !== verifyPassword) {
-      return res.json({
-        error: null,
-        validationErrors: {
-          verifyPassword: 'Your passwords do not match.'
-        }
-      });
-    }
-
-    bcrypt.genSalt(bcryptRounds, function(err, salt) {
-      if (err) {
-        return renderError(res, err);
-      }
-
-      bcrypt.hash(password, salt, function(err, hash) {
+  app.post('/api/log_out', function(req, res) {
+    if (req.signedCookies.sessionId) {
+      models.Session.remove({ _id: req.signedCookies.sessionId }, function(err) {
         if (err) {
           return renderError(res, err);
         }
 
-        var user = new models.User({
-          username: username,
-          usernameLowercase: username.toLowerCase(),
-          passwordHash: hash,
-          passwordSalt: salt
+        res.clearCookie('sessionId');
+        return res.json({
+          error: null
         });
+      });
+    }
+  });
 
-        user.save(function(err) {
-          if (err) {
-            if (err.code === 11000) {
-              return res.json({
-                error: null,
-                validationErrors: {
-                  username: 'That username is already taken.'
-                }
-              });
-            }
+  app.post('/api/check_username', function(req, res) {
+    var username = req.body.username.replace(/^\s+|\s+$/g, '');
 
-            return renderError(res, err);
-          }
+    models.User.findOne({
+        usernameLowercase: username.toLowerCase()
+      }, function(err, user) {
+      if (err) {
+        return renderError(res, err);
+      }
 
-          var session = new models.Session({
-            data: { userId: user._id }
-          });
-
-          session.save(function(err) {
-            if (err) {
-              return renderError(res, err);
-            }
-
-            res.cookie('sessionId', session.id, {
-              signed: true,
-              maxAge: constants.sessionTTL * 1000
-            });
-
-            return res.json({
-              error: null,
-              validationErrors: { },
-              user: getUserData(user)
-            });
-          });
-        });
+      return res.json({
+        error: null,
+        result: (user === null)
       });
     });
   });
@@ -324,18 +349,9 @@ exports.config = function(app) {
 
   app.post('/api/update_password', function(req, res) {
     auth(req, res, function(session, user) {
-      var oldPassword = req.body.oldPassword;
       var newPassword = req.body.newPassword;
       var verifyPassword = req.body.verifyPassword;
-
-      if (oldPassword === '') {
-        return res.json({
-          error: null,
-          validationErrors: {
-            oldPassword: 'Please enter your old password.'
-          }
-        });
-      }
+      var oldPassword = req.body.oldPassword;
 
       if (newPassword === '') {
         return res.json({
@@ -360,6 +376,15 @@ exports.config = function(app) {
           error: null,
           validationErrors: {
             verifyPassword: 'Your passwords do not match.'
+          }
+        });
+      }
+
+      if (oldPassword === '') {
+        return res.json({
+          error: null,
+          validationErrors: {
+            oldPassword: 'Please enter your old password.'
           }
         });
       }
@@ -424,23 +449,6 @@ exports.config = function(app) {
             user: getUserData(user)
           });
         });
-      });
-    });
-  });
-
-  app.post('/api/check_username', function(req, res) {
-    var username = req.body.username.replace(/^\s+|\s+$/g, '');
-
-    var results = models.User.findOne({
-        usernameLowercase: username.toLowerCase()
-      }, function(err, user) {
-      if (err) {
-        return renderError(res, err);
-      }
-
-      return res.json({
-        error: null,
-        result: (user === null)
       });
     });
   });
