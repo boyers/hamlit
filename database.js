42e9e604a7966925e3f579a176559d50cd062dfd
Stephan Boyer <stephan@stephanboyer.com>
2014-12-01 18:44:02 -0500

    no longer crashing if database is down at startup

--- database.js
+++ database.js
@@ -1,27 +1,12 @@
+var mongoose = require('mongoose');
+
 exports.connect = function(done) {
-  var database_url = process.env.DATABASE_URL;
-  if (!database_url) {
-    if (process.env.NODE_ENV === 'production') {
-      console.error('Set the DATABASE_URL environment variable for production.');
-      process.exit(1);
-    } else {
-      database_url = 'mongodb://localhost/hamlit';
-    }
-  }
-
-  var mongoose = require('mongoose');
-  mongoose.connect(database_url);
-
   mongoose.connection.on('connecting', function() {
     console.log('Database connecting...');
   });
 
   mongoose.connection.on('connected', function() {
     console.log('Database connected.');
-  });
-
-  mongoose.connection.once('open', function() {
-    done();
   });
 
   mongoose.connection.on('disconnecting', function() {
@@ -47,4 +32,31 @@ exports.connect = function(done) {
   mongoose.connection.on('fullsetup', function() {
     console.log('Database connected to all replicas.');
   });
+
+  mongoose.connection.once('open', function() {
+    done();
+  });
+
+  var database_url = process.env.DATABASE_URL;
+  if (!database_url) {
+    if (process.env.NODE_ENV === 'production') {
+      console.error('Set the DATABASE_URL environment variable for production.');
+      process.exit(1);
+    } else {
+      database_url = 'mongodb://localhost/hamlit';
+    }
+  }
+
+  var connect = function() {
+    mongoose.connect(database_url, function(err) {
+      if (err) {
+        console.error('Database error:', err.stack);
+        setTimeout(function() {
+          connect();
+        }, 1000);
+      }
+    });
+  };
+
+  connect();
 };
