9af5e4102e79d1ede4db078e881a5a08dbdb125c
Stephan Boyer <stephan@stephanboyer.com>
2014-12-03 13:19:09 -0500

    adding keepalive mechanism for database connection

--- database.js
+++ database.js
@@ -32,6 +32,8 @@ mongoose.connection.on('fullsetup', function() {
   console.log('Database connected to all replicas.');
 });
 
+// Determine the database connection settings from the
+// environment variables.
 exports.databaseURL = process.env.DATABASE_URL;
 if (!exports.databaseURL) {
   if (process.env.NODE_ENV === 'production') {
@@ -42,12 +44,31 @@ if (!exports.databaseURL) {
   }
 }
 
+// Ping the database every few seconds.
+// If the database connection fails, the driver often
+// doesn't know about it until a query attempted.
+// This "keepalive" mechanism helps the driver detect when
+// the connection is down in the absence of active queries.
+setInterval(function() {
+  if (mongoose.connection.readyState === 1) {
+    mongoose.connection.db.admin().ping(function(err, result) {
+      if (err) {
+        console.error('Database ping error:', err.stack);
+      }
+    });
+  }
+}, 30000);
+
 exports.connect = function(done) {
   mongoose.connection.once('open', function() {
     done();
   });
 
   // Start connecting only if we're disconnected.
+  // If the database doesn't connect on the first try,
+  // we retry once every second. Once we are connected, mongodb's
+  // auto_reconnect mechanism will handle future connection
+  // failures.
   if (mongoose.connection.readyState === 0) {
     var connect = function() {
       mongoose.connect(exports.databaseURL, function(err) {
